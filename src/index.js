3914abc03cf191e09922e4fe3b219a8cc131e940
Iryna Paliakova <40804746+syirok@users.noreply.github.com>
2020-02-24 11:05:23 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  let mini = 0;
+  for (let i in array) {
+    if (array[i] < mini) {mini = array[i];}
+  }
+  return mini;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxi = 0;
+  for (let i in array) {
+    if (array[i] > maxi) {maxi = array[i]}
+  }
+  return maxi;
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let sum = 0;
+  if (!Array.isArray(array)) {return 0;}
+  else if (array.length == 0) {return 0;}
+  else {
+    for (let i in array) {
+            sum += array[i];
+        }
+      return sum/array.length;
+  }
+    
+  }
